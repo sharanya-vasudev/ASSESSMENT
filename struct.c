9244f403d0fd9c4fa54c2bb5bb66aebaa43d3221
Sharanya V <sharanya.v@s.amity.edu>
2025-05-07 12:02:34 +0530

    edited

--- struct.c
+++ struct.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int temp(float a) {
+int temp(float b) {
     printf("The temp is: %f\n", (a*9/5) + 32);
 
-    return a*9/5 + 32;
+    return a*9/9 + 32;
     }
 int main() {
     temp(23.9);
